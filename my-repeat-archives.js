044bcd569f727116ecd74f191cec72be6381c939
Tolga Topkaç <tolgatopkac09@gmail.com>
2021-06-30 13:54:29 +0300

    update my-repeat-archives.js

---
+++ my-repeat-archives.js
@@ -0,0 +1,36 @@
+// first-lecture-notes.js
+    //-- Escape Sequences in Strings
+
+// third-lecture-notes.js
+    //--Global Scope and Functions
+    //--Local Scope and Functions
+    //--Global vs. Local Scope in Functions
+
+// fourth-lecture-notes.js
+    //-- Returning Early Pattern from Functions
+
+// fifth-lecture-notes.js
+    //--Accessing Object Properties with Variables
+    //--Using Object for Lookups
+    //--Testing Objects for Properties
+    //--Manipulating Complex Objects
+    //--Accessing Nested Arrays
+    //--Record Collection(EXERCISE)
+
+// sixth-lecture-notes.js
+    //--Nesting For Loops 
+    //--Profile Lookup
+
+// seventh-lecture-notes.js
+    // --Generate Random Whole Numbers within a Range   
+    // --Write Higher Order Arrow Functions 
+    // --Use the Rest Operator with Function Parameters 
+    // --Use Destructuring Assignment to Assign Variables from Objects 
+    // --Destructuring Assignment with Nested Objects 
+    // --Use Destructuring Assignment to Assign Variables from Arrays
+    // --Use Destructuring Assignment with the Rest Operator
+    // --Use Destructuring Assignment to Pass an Object as a Function's Parameters
+    // --MakeList(EXERCISE)
+    // --Use class Syntax to Define a Constructor Function
+    // --Use getters and setters to Control Access to an Object.
+    // --makeClass() Thermostat Exercise
